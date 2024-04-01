f4948c7010cf342c4ecc825096bc0d6184e03ce9
horsicq <horsicq@gmail.com>
2024-04-01 20:10:04 +0200

    Add new file(s): 2024-04-01

---
+++ xbreakpointswidget.cmake
@@ -0,0 +1,10 @@
+include_directories(${CMAKE_CURRENT_LIST_DIR})
+
+set(XBREAKPOINTSWIDGET_SOURCES
+    ${CMAKE_CURRENT_LIST_DIR}/dialogeditbreakpoint.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/dialogeditbreakpoint.h
+    ${CMAKE_CURRENT_LIST_DIR}/dialogeditbreakpoint.ui
+    ${CMAKE_CURRENT_LIST_DIR}/xbreakpointswidget.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/xbreakpointswidget.h
+    ${CMAKE_CURRENT_LIST_DIR}/xbreakpointswidget.ui
+)
